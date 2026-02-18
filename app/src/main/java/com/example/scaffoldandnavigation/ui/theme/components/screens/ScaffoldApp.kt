c0cd09816522063366a681c125fefb4c79aab4b6
Minoovn <mvahedin24@students.oamk.fi>
2026-02-18 23:50:34 +0200

    ui

--- app/src/main/java/com/example/scaffoldandnavigation/ui/theme/components/screens/ScaffoldApp.kt
+++ app/src/main/java/com/example/scaffoldandnavigation/ui/theme/components/screens/ScaffoldApp.kt
@@ -1,16 +1,20 @@
 package com.example.scaffoldandnavigation.ui.screens
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun ScaffoldApp() {
-
     val navController = rememberNavController()
 
-    NavHost(navController, startDestination = "home") {
-        composable("home") { MainScreen(navController) }
-        composable("info") { InfoScreen(navController) }
-        composable("settings") { SettingsScreen(navController) }
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable(route = "home") { MainScreen(navController) }
+        composable(route = "info") { InfoScreen(navController) }
+        composable(route = "settings") { SettingsScreen(navController) }
     }
 }
--- app/src/main/java/com/example/scaffoldandnavigation/ui/theme/components/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/scaffoldandnavigation/ui/theme/components/screens/SettingsScreen.kt
@@ -1,17 +1,21 @@
 package com.example.scaffoldandnavigation.ui.screens
 
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.padding
 import androidx.navigation.NavController
 import com.example.scaffoldandnavigation.ui.components.ScreenTopBar
 
 @Composable
 fun SettingsScreen(navController: NavController) {
     Scaffold(
-        topBar = { ScreenTopBar("Settings", navController) }
-    ) { padding ->
-        Text("Settings Screen", Modifier.padding(padding))
+        topBar = { ScreenTopBar("Settings", navController) },
+    ) { innerPadding ->
+        Text(
+            text = "Settings Screen",
+            modifier = Modifier.padding(innerPadding)
+        )
     }
 }
